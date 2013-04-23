3ee993067e8871f0bec2f57266b7900bf3924419
lodengo <ddl5200@gmail.com>
2013-04-23 12:09:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,44 +11,39 @@ var app = express();
 
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 80);
-  //app.set('views', __dirname + '/views');
-  //app.set('view engine', 'ejs');
-  //app.use(express.favicon());
+  app.set('port', process.env.PORT || 80);  
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
-  app.use(app.router);
-  //app.use(express.static(path.join(__dirname, 'public')));
+  app.use(app.router);  
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
-});
+})
 
-var weibo2 = require('./setting').weibo2;
-weibo2.route(app);
+var OAuth2 = require('./OAuth2')(app);
+var weibo2 = new OAuth2(require('./setting').weibo_conf);
 weibo2.setAuthorizationCallback(function(req, res, error, data){
 	if(error){
-		res.json(data);
+		res.json({error:error, data:data});
 	}
 	else{
 		res.send('<a href="/statuses/public_timeline">/statuses/public_timeline</a> </p><a href="/statuses/friends_timeline">/statuses/friends_timeline</a> </p><a href="/statuses/home_timeline">/statuses/home_timeline</a> </p>');
 	}
 });
 
-var douban = require('./setting').douban;
-douban.route(app);
+var douban = new OAuth2(require('./setting').douban_conf);
 douban.setAuthorizationCallback(function(req, res, error, data){
 	if(error){
-		res.json(data);
+		res.json({error:error, data:data});
 	}
 	else{
 		res.send('<a href="/movie/nowplaying">/movie/nowplaying</a> </p><a href="/movie/coming">/movie/coming</a> ');
 	}
-});
+}); 
 
 app.get('/', function(req, res){	
 	res.send('<a href="/weibo">weibo</a></p><a href="/douban">douban</a>');
