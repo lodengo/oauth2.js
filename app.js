72149029f2c8973b54c8a3a1177ac15a2bfbb910
lodengo <ddl5200@gmail.com>
2013-04-25 09:52:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,9 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 })
 
-var OAuth2 = require('./OAuth2')(app);
+var OAuth2 = require('./OAuth2');
 var weibo2 = new OAuth2(require('./setting').weibo_conf);
+weibo2.route(app);
 weibo2.setAuthorizationCallback(function(req, res, error, data){
 	if(error){
 		res.json({error:error, data:data});
@@ -36,6 +37,7 @@ weibo2.setAuthorizationCallback(function(req, res, error, data){
 });
 
 var douban = new OAuth2(require('./setting').douban_conf);
+douban.route(app);
 douban.setAuthorizationCallback(function(req, res, error, data){
 	if(error){
 		res.json({error:error, data:data});
@@ -43,7 +45,8 @@ douban.setAuthorizationCallback(function(req, res, error, data){
 	else{
 		res.send('<a href="/movie/nowplaying">/movie/nowplaying</a> </p><a href="/movie/coming">/movie/coming</a> ');
 	}
-}); 
+});  
+
 
 app.get('/', function(req, res){	
 	res.send('<a href="/weibo">weibo</a></p><a href="/douban">douban</a>');
@@ -56,6 +59,7 @@ app.get('/weibo', function(req, res){
 app.get('/douban', function(req, res){
 	douban.obtainingAuthorization(req, res);
 });
+
 
 app.get('/statuses/public_timeline', function(req, res){
 	weibo2.accessProtectedResource(req, res, 'GET', 'https://api.weibo.com/2/statuses/public_timeline.json', {}, function(error, data){
@@ -84,6 +88,7 @@ app.get('/movie/coming', function(req, res){
 		res.json(data);
 	});
 });
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
